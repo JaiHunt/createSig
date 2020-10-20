ae345e1390488336501217ddf945e31ee1c24bad
JaiHunt <Jai_hunt@outlook.com>
2020-10-20 15:52:45 +1000

    1st attempt

--- createSigs/signature.cpp
+++ createSigs/signature.cpp
@@ -4,26 +4,17 @@
 #include <math.h>
 #include "uthash.h"
 #include <chrono>
-#include <thread>
-#include <mutex>
-#include <omp.h>
 
 typedef unsigned char byte;
 
 #define SIGNATURE_LEN 64
-#define N 4 
 
 int DENSITY = 21;
 int PARTITION_SIZE;
 
 int inverse[256];
-const char* alphabet = "CSTPAGNDEQHRKMILVFYW"; // unique animo acids
+const char* alphabet = "CSTPAGNDEQHRKMILVFYW";
 
-/* CSTPAGNDEQHRKMILVFYW = 01234567890123456789
-   e.g. MKI = 0, -1, 1, 0, 0, 1, 1, -1, 0, 0, -1
-   M = 13, K = 12, I = 14 = 13*20^2 + 12*20^1 + 1*20^0
-   This number will represent the specific kmer where that is 0 between 20^WORDLEN
-*/
 
 void seed_random(char* term, int length);
 short random_num(short max);
@@ -34,175 +25,159 @@ int doc_sig[SIGNATURE_LEN];
 int WORDLEN;
 FILE* sig_file;
 
+typedef struct
+{
+    char term[100];
+    short sig[SIGNATURE_LEN];
+    UT_hash_handle hh;
+} hash_term;
+
+
 short* compute_new_term_sig(char* term, short* term_sig)
 {
-	seed_random(term, WORDLEN); // rand nums are seeded based on the term itself and WORDLEN
+    seed_random(term, WORDLEN);
 
-	int non_zero = SIGNATURE_LEN * DENSITY / 100; // set everything to 0
+    int non_zero = SIGNATURE_LEN * DENSITY / 100;
 
+    int positive = 0;
+    while (positive < non_zero / 2)
+    {
+        short pos = random_num(SIGNATURE_LEN);
+        if (term_sig[pos] == 0)
+        {
+            term_sig[pos] = 1;
+            positive++;
+        }
+    }
 
-	// 10.5% +ve
-	int positive = 0;
-	while (positive < non_zero / 2)
-	{
-		short pos = random_num(SIGNATURE_LEN);
-		if (term_sig[pos] == 0)
-		{
-			term_sig[pos] = 1;
-			positive++;
-		}
-	}
-
-	// 10.5% -ve
-	int negative = 0;
-	while (negative < non_zero / 2)
-	{
-		short pos = random_num(SIGNATURE_LEN);
-		if (term_sig[pos] == 0)
-		{
-			term_sig[pos] = -1;
-			negative++;
-		}
-	}
-	return term_sig;
+    int negative = 0;
+    while (negative < non_zero / 2)
+    {
+        short pos = random_num(SIGNATURE_LEN);
+        if (term_sig[pos] == 0)
+        {
+            term_sig[pos] = -1;
+            negative++;
+        }
+    }
+    return term_sig;
 }
 
-#pragma omp parallel
+short* find_sig(char* term, hash_term** vocab_)
 {
-	hash_term* vocab = NULL;
+    hash_term* entry;
+    auto vocab = *vocab_;
 
-	#pragma omp parallel for
-		for (int i = 0; i < N; i++) {
-			run_parahash(&vocab);
-		}
-	}
+    HASH_FIND(hh, vocab, term, WORDLEN, entry);
+    if (entry == NULL)
+    {
+        entry = (hash_term*)malloc(sizeof(hash_term));
+        strncpy_s(entry->term, sizeof(entry->term), term, WORDLEN);
+        memset(entry->sig, 0, sizeof(entry->sig));
+        compute_new_term_sig(term, entry->sig);
+        HASH_ADD(hh, vocab, term, WORDLEN, entry);
+    }
+    
+    *vocab_ = vocab;
+    return entry->sig;
 }
 
-void run_parahash(hash_term** vocab)
-{
-	hash_term* entry;
-	HASH_FIND(hh, *vocab, term, WORDLEN, entry);
-	HASH_ADD(hh, *vocab, term, WORDLEN, entry);
-}
 
-/*
-	hash find has the highest hotpath and cpu usage, one possible way to fix this is having to
-	find functions. Where hash find -> returns null -> lock -> hash find -> then adds the entry.
-	While running different hash tables concurrently on separate threads. To then combine the entries
-	towards the end.
-*/
-short* find_sig(char* term, hash_term** vocab)
+void signature_add(char* term, hash_term** vocab)
 {
-	hash_term* entry; // cache that remembers kmers used before
-
-	HASH_FIND(hh, *vocab, term, WORDLEN, entry); // hotpath
-	if (entry == NULL)
-	{
-		entry = (hash_term*)malloc(sizeof(hash_term));
-		strncpy_s(entry->term, sizeof(entry->term), term, WORDLEN);
-		memset(entry->sig, 0, sizeof(entry->sig));
-		compute_new_term_sig(term, entry->sig);
-		HASH_ADD(hh, *vocab, term, WORDLEN, entry);
-	}
-	return entry->sig;
-}
-
-void signature_add(char* term)
-{
-	short* term_sig = find_sig(term, );
-	for (int i = 0; i < SIGNATURE_LEN; i++)
-		doc_sig[i] += term_sig[i];
+    short* term_sig = find_sig(term, vocab);
+    for (int i = 0; i < SIGNATURE_LEN; i++)
+        doc_sig[i] += term_sig[i];
 }
 
 int doc = 0;
 
-void compute_signature(char* sequence, int length)
+void compute_signature(char* sequence, int length, hash_term** vocab)
 {
-	memset(doc_sig, 0, sizeof(doc_sig)); // sets total to all 0s
+    memset(doc_sig, 0, sizeof(doc_sig));
+    for (int i = 0; i < length - WORDLEN + 1; i++)
+        signature_add(sequence + i, vocab);
+    
+    // save document number to sig file
+    fwrite(&doc, sizeof(int), 1, sig_file);
 
-	// goes through each kmer that makes the parition and adds them to the above doc sig
-	for (int i = 0; i < length - WORDLEN + 1; i++)
-		signature_add(sequence + i); // Adds a complete row each loop iteration
-
-	// save document number to sig file
-	fwrite(&doc, sizeof(int), 1, sig_file);
-
-	// flatten and output to sig file
-	for (int i = 0; i < SIGNATURE_LEN; i += 8)
-	{
-		byte c = 0;
-		for (int j = 0; j < 8; j++)
-			c |= (doc_sig[i + j] > 0) << (7 - j);
-		fwrite(&c, sizeof(byte), 1, sig_file);
-	}
+    // flatten and output to sig file
+    for (int i = 0; i < SIGNATURE_LEN; i += 8)
+    {
+        byte c = 0;
+        for (int j = 0; j < 8; j++)
+            c |= (doc_sig[i + j] > 0) << (7 - j);
+        fwrite(&c, sizeof(byte), 1, sig_file);
+    }
 }
 
 #define min(a,b) ((a) < (b) ? (a) : (b))
 
 void partition(char* sequence, int length)
 {
-	int i = 0;
-	do
-	{
-		compute_signature(sequence + i, min(PARTITION_SIZE, length - i)); // function computes sig for just one partition
-		i += PARTITION_SIZE / 2; // increments start of the partition by half the partition size (half overlap between one partition and the next)
-	} while (i + PARTITION_SIZE / 2 < length);
-	doc++;
+    #pragma omp parallel
+    {
+        static hash_term* vocab = NULL;
+        #pragma omp for
+        for (int i = 0; i < (length + ((-1/2) * PARTITION_SIZE)); i += PARTITION_SIZE / 2) {
+            compute_signature(sequence+i, min(PARTITION_SIZE, length-i), &vocab);
+        }
+        doc++;
+    }
 }
 
 int power(int n, int e)
 {
-	int p = 1;
-	for (int j = 0; j < e; j++)
-		p *= n;
-	return p;
+    int p = 1;
+    for (int j = 0; j < e; j++)
+        p *= n;
+    return p;
 }
 
 int main(int argc, char* argv[])
 {
-	//const char* filename = "qut2.fasta";
-	const char* filename = "qut3.fasta";
+    //const char* filename = "qut2.fasta";
+    const char* filename = "qut3.fasta";
 
-	WORDLEN = 3;
-	PARTITION_SIZE = 16;
-	int WORDS = power(20, WORDLEN); // only 20 letters of the aplphabet are used, hence base 20
+    WORDLEN = 3;
+    PARTITION_SIZE = 16;
+    int WORDS = power(20, WORDLEN);
 
-	for (int i = 0; i < strlen(alphabet); i++)
-		inverse[alphabet[i]] = i; // for each position in the aplhabet, the inverse is calculated to be position i
+    for (int i = 0; i < strlen(alphabet); i++)
+        inverse[alphabet[i]] = i;
 
-	auto start = std::chrono::high_resolution_clock::now();
+    auto start = std::chrono::high_resolution_clock::now();
 
-	FILE* file;
-	errno_t OK = fopen_s(&file, filename, "r");
+    FILE* file;
+    errno_t OK = fopen_s(&file, filename, "r");
 
-	if (OK != 0)
-	{
-		fprintf(stderr, "Error: failed to open file %s\n", filename);
-		return 1;
-	}
+    if (OK != 0)
+    {
+        fprintf(stderr, "Error: failed to open file %s\n", filename);
+        return 1;
+    }
 
-	/* writing to file */
-	char outfile[256];
-	sprintf_s(outfile, 256, "%s.part%d_sigs%02d_%d", filename, PARTITION_SIZE, WORDLEN, SIGNATURE_LEN);
-	fopen_s(&sig_file, outfile, "w");
+    char outfile[256];
+    sprintf_s(outfile, 256, "%s.part%d_sigs%02d_%d", filename, PARTITION_SIZE, WORDLEN, SIGNATURE_LEN);
+    fopen_s(&sig_file, outfile, "w");
 
-	char buffer[10000];
-	while (!feof(file))
-	{
-		fgets(buffer, 10000, file); // skip meta data line - every first or so line that contains irrelevant info  
-		fgets(buffer, 10000, file);
-		int n = (int)strlen(buffer) - 1; // reads the sig data and returns n characters 
-		buffer[n] = 0;
-		partition(buffer, n); // Written out here
-	}
-	fclose(file);
+    char buffer[10000];
+    while (!feof(file))
+    {
+        fgets(buffer, 10000, file); // skip meta data line
+        fgets(buffer, 10000, file);
+        int n = (int)strlen(buffer) - 1;
+        buffer[n] = 0;
+        partition(buffer, n);
+    }
+    fclose(file);
 
-	fclose(sig_file);
+    fclose(sig_file);
 
-	auto end = std::chrono::high_resolution_clock::now();
-	std::chrono::duration<double> duration = end - start;
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
 
-	printf("%s %f seconds\n", filename, duration.count());
+    printf("%s %f seconds\n", filename, duration.count());
 
-	return 0;
+    return 0;
 }
